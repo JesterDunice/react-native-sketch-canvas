8c0165053930e00adb47a6a06fdb077adace53e1
JesterDunice <n.dudin@dunice.net>
2020-03-20 17:29:08 +0300

    Change deprecated lifecycle methods to UNSAFE

--- src/SketchCanvas.js
+++ src/SketchCanvas.js
@@ -94,7 +94,7 @@ class SketchCanvas extends React.Component {
     this.state.text = this._processText(props.text ? props.text.map(t => Object.assign({}, t)) : null)
   }
 
-  componentWillReceiveProps(nextProps) {
+  UNSAFE_componentWillReceiveProps(nextProps) {
     this.setState({
       text: this._processText(nextProps.text ? nextProps.text.map(t => Object.assign({}, t)) : null)
     })
@@ -154,7 +154,7 @@ class SketchCanvas extends React.Component {
     }
   }
 
-  componentWillMount() {
+  UNSAFE_componentWillMount() {
     this.panResponder = PanResponder.create({
       // Ask to be the responder:
       onStartShouldSetPanResponder: (evt, gestureState) => true,
